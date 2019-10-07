bebc43224ef2d8b52512218c4d14fbcc50141dc2
Bernadette Murphy <bernieh1012008@live.co.uk>
2019-10-07 21:43:26 +0100

    Update MainActivity.kt

--- app/src/main/java/org/wit/myapplication/MainActivity.kt
+++ app/src/main/java/org/wit/myapplication/MainActivity.kt
@@ -3,10 +3,10 @@ package org.wit.myapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    @Override fun onCreate(savedInstanceState) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
     }
 }
